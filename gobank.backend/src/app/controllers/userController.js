2c971b50c901b039d5645eb0f9590f611ee7474c
Vítor Ribeiro <furiadev2020@gmail.com>
2021-11-08 15:40:04 -0300

    add userController to app

---
+++ gobank.backend/src/app/controllers/userController.js
@@ -0,0 +1,7 @@
+export default {
+  async auth(req, res) {
+    // Waiting for Gabriel's code.
+
+    return res.status(200).json({ isAuthenticated: true });
+  },
+};
--- gobank.backend/src/config/server.js
+++ gobank.backend/src/config/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import userController from '../app/controllers/userController';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.options('*', cors());
+app.use('/gb/v1/auth', userController.auth);
 
 export default app;
