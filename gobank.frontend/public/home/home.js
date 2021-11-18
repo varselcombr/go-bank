12b2c03fc6267cd8d3c8795191d63b465e98c0ab
vini54 <vinioli544@gmail.com>
2021-11-18 09:38:58 -0300

    finish carousel

--- gobank.frontend/public/home/home.js
+++ gobank.frontend/public/home/home.js
@@ -2,20 +2,34 @@ const botao1 = document.getElementById("cartao1");
 const botao2 = document.getElementById("cartao2");
 const card1 = document.getElementById("creditCard1");
 const card2 = document.getElementById("creditCard2");
+const carousel = document.querySelector('.carousel')
 
+carousel.addEventListener('scroll', activeBtns)
 
 function moveCard2() {
-  card1.style.transform = "translateX(-280px)"
-  card2.style.transform = "translateX(-280px)"
+  carousel.scroll({
+    left: 250,
+    behavior: 'smooth'
+  })
 
-  botao1.style.background = "#D0D5DB"
-  botao2.style.background = "#873F8C"
+  activeBtns()
 }
 
 function moveCard1() {
-  card1.style.transform = "translateX(10px)"
-  card2.style.transform = "translateX(10px)"
+  carousel.scroll({
+    left: -250,
+    behavior: "smooth"
+  })
 
-  botao1.style.background = "#873F8C"
-  botao2.style.background = "#D0D5DB"
+  activeBtns()
+}
+
+function activeBtns(){
+  if(carousel.scrollLeft > 200 ){
+    botao1.style.background = "#D0D5DB"
+    botao2.style.background = "#873F8C"
+  }else if(carousel.scrollLeft <= 10){
+    botao1.style.background = "#873F8C"
+    botao2.style.background = "#D0D5DB"
+  }
 }
