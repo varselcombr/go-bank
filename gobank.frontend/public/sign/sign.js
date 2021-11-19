d2b8a4eef38ed00aa4d787646c155a5f8d8bc91a
vini54 <vinioli544@gmail.com>
2021-11-19 10:26:25 -0300

    update sign chat

--- gobank.frontend/public/sign/sign.js
+++ gobank.frontend/public/sign/sign.js
@@ -5,7 +5,9 @@ let sendBtn = document.querySelector('#sendMsg')
 const messages = {
    msg1: "Olá!",
    msg2: "Vamos começar o processo de abertura da sua conta.",
-   msg3: "Como deseja ser chamado?"
+   msg3: "Como deseja ser chamado?",
+   msg4: `Para continuar você precisa estar de acordo com os nossos Termos de Uso e política de privacidade <a href="#" class="terms">ABRIR TERMOS DE USO</a>`,
+   msg5: 'Agora, qual é o número do seu CPF?'
 }
 
 function scrollDown(){
@@ -19,13 +21,13 @@ function scrollDown(){
 }
 
 function sendUserMsg(callback){
-   if(userInput.value.length > 3){
+   if(userInput.value.length > 0){
       let userMsg = document.createElement('div')
       userMsg.classList.add('userMsg')
       userMsg.innerHTML = `${userInput.value}`
+      chatContainer.appendChild(userMsg)
       callback(userInput.value)
       userInput.value = ''
-      chatContainer.appendChild(userMsg)
       scrollDown()
    }
 }
@@ -52,16 +54,70 @@ function sendSistemMessages1(){
    }, 3000)
 }
 
+const handleKeyup1 = (e) => {
+   if(e.which === 13){sendUserMsg(getName)}
+}
+const handleKeyup2 = (e) => {
+   if(e.which === 13){sendUserMsg(getConfirm)}
+}
+const handleKeyup3 = (e) => {
+   if(e.which === 13){sendUserMsg(getCpf)}
+}
+
+const handleClick1 = () => {
+   sendUserMsg(getName)
+}
+const handleClick2 = () => {
+   sendUserMsg(getConfirm)
+}
+const handleClick3 = () => {
+   sendUserMsg(getCpf)
+}
+
 function getName(value){
    const name = value
    console.log(name)
+
+   userInput.removeEventListener('keyup', handleKeyup1)
+   sendBtn.removeEventListener('click', handleClick1)
+   requestConfirm()
+}
+
+function requestConfirm(){
+   setTimeout(function(){
+      sendSistemMsg(messages.msg4)
+      userInput.addEventListener('keyup', handleKeyup2)
+      sendBtn.addEventListener('click', handleClick2)
+   }, 1000)
+}
+
+function getConfirm(value){
+   const confirm = value
+   console.log(confirm)
+
+   userInput.removeEventListener('keyup', handleKeyup2)
+   sendBtn.removeEventListener('click', handleClick2)
+   requestCpf()
+}
+
+function requestCpf(){
+   setTimeout(function(){
+      sendSistemMsg(messages.msg5)
+      userInput.addEventListener('keyup', handleKeyup3)
+      sendBtn.addEventListener('click', handleClick3)
+   }, 1000)
+}
+
+function getCpf(value){
+   const CPF = value
+   console.log(CPF)
 }
 
 function initChat(){
    sendSistemMessages1()
    setTimeout(function(){
-      userInput.addEventListener('keyup', e => {if(e.which === 13){sendUserMsg(getName)}})
-      sendBtn.addEventListener('click', () => {sendUserMsg(getName)})
+      userInput.addEventListener('keyup', handleKeyup1)
+      sendBtn.addEventListener('click', handleClick1)
       console.log('finish')
    }, 3000)
 }
