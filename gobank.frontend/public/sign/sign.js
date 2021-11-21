6adfe014ea067305718187093eb3e2fc49b7332e
vini54 <vinioli544@gmail.com>
2021-11-21 18:26:19 -0300

    update sign chat

--- gobank.frontend/public/sign/sign.js
+++ gobank.frontend/public/sign/sign.js
@@ -1,5 +1,6 @@
 let chatContainer = document.querySelector('.chatContainer')
 let userInput = document.querySelector('#inputMsg')
+let inputBox = document.querySelector('.userInput')
 let sendBtn = document.querySelector('#sendMsg')
 
 const messages = {
@@ -18,6 +19,7 @@ const messages = {
    msg13: 'gostariamos de uma foto para apresentarmos no seu perfil!',
    msg131: 'informe um link de acesso remoto a sua foto (Temporário)',
    msg14: 'Agora, digite uma senha númerica de 8 dígitos para acessar sua conta',
+   msg15: 'Para finalizar, digite uma palavra-chave ou frase para usarmos no caso de perda de senha de acesso',
 }
 
 let userInfos = {}
@@ -36,12 +38,22 @@ function sendUserMsg(callback){
    if(userInput.value.length > 0){
       let userMsg = document.createElement('div')
       userMsg.classList.add('userMsg')
-      userMsg.innerHTML = `${userInput.value}`
+      if(callback == getPassword){
+         for (const i of userInput.value) {
+            userMsg.innerText += `*`
+         }
+      }else{
+         userMsg.innerHTML = `${userInput.value}`
+      }
       chatContainer.appendChild(userMsg)
       callback(userInput.value)
       userInput.value = ''
       scrollDown()
    }
+}
+
+function alertUser(){
+   inputBox.style.border = '1px solid red'
 }
 
 function sendSistemMsg(msg){
@@ -97,6 +109,9 @@ const handleKeyup9 = (e) => {
 const handleKeyup10 = (e) => {
    if(e.which === 13){sendUserMsg(getPassword)}
 }
+const handleKeyup11 = (e) => {
+   if(e.which === 13){sendUserMsg(getRecovery)}
+}
 
 const handleClick1 = () => {
    sendUserMsg(getName)
@@ -128,121 +143,102 @@ const handleClick9 = () => {
 const handleClick10 = () => {
    sendUserMsg(getPassword)
 }
+const handleClick11 = () => {
+   sendUserMsg(getRecovery)
+}
 
 function getName(value){
-   const name = value
-   console.log(name)
-
-   userInfos.username = name
-   console.log(userInfos)
-
+   userInfos.username = value
    userInput.removeEventListener('keyup', handleKeyup1)
    sendBtn.removeEventListener('click', handleClick1)
    requestFname()
 }
 
 function requestFname(){
-   setTimeout(function(){
+   const request1 = setTimeout(function(){
       sendSistemMsg(messages.msg4)
+      clearTimeout(request1)
    },1000)
-   setTimeout(function(){
+   const request2 = setTimeout(function(){
       sendSistemMsg(messages.msg5)
       userInput.setAttribute('placeholder', 'Digite seu primeiro nome')
       userInput.addEventListener('keyup', handleKeyup2)
       sendBtn.addEventListener('click', handleClick2)
+      clearTimeout(request2)
    }, 2000)
 }
 
 function getFname(value){
-   const fName = value
-   console.log(fName)
-
    userInfos.personal = {}
-   userInfos.personal.fname = fName
-   console.log(userInfos)
-
+   userInfos.personal.fname = value
    userInput.removeEventListener('keyup', handleKeyup2)
    sendBtn.removeEventListener('click', handleClick2)
    requestLname()
 }
 
 function requestLname(){
-   setTimeout(function(){
+   const request = setTimeout(function(){
       sendSistemMsg(messages.msg6)
       userInput.setAttribute('placeholder', 'Digite seu(s) sobrenome(s)')
       userInput.addEventListener('keyup', handleKeyup3)
       sendBtn.addEventListener('click', handleClick3)
+      clearTimeout(request)
    }, 1000)
 }
 
 function getLname(value){
-   const Lname = value
-   console.log(Lname)
-
-   userInfos.personal.lname =Lname
-   console.log(userInfos)
-
+   userInfos.personal.lname = value
    userInput.removeEventListener('keyup', handleKeyup3)
    sendBtn.removeEventListener('click', handleClick3)
    requestAge()
 }
 
 function requestAge(){
-   setTimeout(function(){
+   const request = setTimeout(function(){
       sendSistemMsg(messages.msg7)
       userInput.setAttribute('placeholder', 'Digite sua idade')
       userInput.addEventListener('keyup', handleKeyup4)
       sendBtn.addEventListener('click', handleClick4)
+      clearTimeout(request)
    }, 1000)
 }
 
 function getAge(value){
-   const age = value
-   console.log(age)
-
-   userInfos.personal.age = age
-   console.log(userInfos)
-
+   userInfos.personal.age = value
    userInput.removeEventListener('keyup', handleKeyup4)
    sendBtn.removeEventListener('click', handleClick4)
    requestConfirm()
 }
 
 function requestConfirm(){
-   setTimeout(function(){
+   const request = setTimeout(function(){
       sendSistemMsg(messages.msg8)
       userInput.setAttribute('placeholder', 'Digite \"Ok\" para continuar')
       userInput.addEventListener('keyup', handleKeyup5)
       sendBtn.addEventListener('click', handleClick5)
+      clearTimeout(request)
    }, 1000)
 }
 
-function getConfirm(value){
-   const confirm = value
-   console.log(confirm)
-
+function getConfirm(){
    userInput.removeEventListener('keyup', handleKeyup5)
    sendBtn.removeEventListener('click', handleClick5)
    requestCpf()
 }
 
 function requestCpf(){
-   setTimeout(function(){
+   const request = setTimeout(function(){
       sendSistemMsg(messages.msg9)
       userInput.setAttribute('placeholder', 'Digite seu CPF')
       userInput.addEventListener('keyup', handleKeyup6)
       sendBtn.addEventListener('click', handleClick6)
+      clearTimeout(request)
    }, 1000)
 }
 
 function getCpf(value){
-   const CPF = value
-   console.log(CPF)
-
-   if(CPF.length === 11){
-      userInfos.cpf = CPF
-      console.log(userInfos)
-
+   if(value.length === 11){
+      userInfos.cpf = value
       userInput.removeEventListener('keyup', handleKeyup6)
       sendBtn.removeEventListener('click', handleClick6)
       requestEmail()
@@ -252,93 +248,102 @@ function getCpf(value){
 }
 
 function requestEmail(){
-   setTimeout(function(){
+   const request = setTimeout(function(){
       sendSistemMsg(messages.msg11)
       userInput.setAttribute('placeholder', 'Digite seu E-mail')
       userInput.setAttribute('type', 'email')
       userInput.addEventListener('keyup', handleKeyup7)
       sendBtn.addEventListener('click', handleClick7)
+      clearTimeout(request)
    }, 1000)
 }
 
 function getEmail(value){
-   const email = value
-   console.log(email)
-
-   userInfos.email = email
-   console.log(userInfos)
-
+   userInfos.email = value
    userInput.removeEventListener('keyup', handleKeyup7)
    sendBtn.removeEventListener('click', handleClick7)
    requestPhone()
 }
 
 function requestPhone(){
-   setTimeout(function(){
+   const request = setTimeout(function(){
       sendSistemMsg(messages.msg12)
       userInput.setAttribute('placeholder', 'Digite seu número')
       userInput.setAttribute('type', 'number')
       userInput.addEventListener('keyup', handleKeyup8)
       sendBtn.addEventListener('click', handleClick8)
+      clearTimeout(request)
    }, 1000)
 }
 
 function getPhone(value){
-   const phone = value
-   console.log(phone)
-
-   userInfos.phone = phone
-   console.log(userInfos)
-
+   userInfos.phone = value
    userInput.removeEventListener('keyup', handleKeyup8)
    sendBtn.removeEventListener('click', handleClick8)
    requestAvatar()
 }
 
 function requestAvatar(){
-   setTimeout(function(){
+   const request1 = setTimeout(function(){
       sendSistemMsg(messages.msg13)
+      clearTimeout(request1)
    }, 1000)
-   setTimeout(function(){
+   const request2 = setTimeout(function(){
       sendSistemMsg(messages.msg131)
       userInput.setAttribute('placeholder', 'Digite um link para sua foto')
       userInput.setAttribute('type', 'text')
       userInput.addEventListener('keyup', handleKeyup9)
       sendBtn.addEventListener('click', handleClick9)
+      clearTimeout(request2)
    }, 2000)
 }
 
 function getAvatar(value){
-   const avatar = value
-   console.log(avatar)
-
-   userInfos.personal.avatar = avatar
-   console.log(userInfos)
-
+   userInfos.personal.avatar = value
    userInput.removeEventListener('keyup', handleKeyup9)
    sendBtn.removeEventListener('click', handleClick9)
    requestPassword()
 }
 
 function requestPassword(){
-   setTimeout(function(){
+   const request = setTimeout(function(){
       sendSistemMsg(messages.msg14)
       userInput.setAttribute('placeholder', 'Digite uma senha')
       userInput.setAttribute('type', 'password')
       userInput.setAttribute('maxlength', '8')
       userInput.addEventListener('keyup', handleKeyup10)
       sendBtn.addEventListener('click', handleClick10)
+      clearTimeout(request)
    }, 1000)
 }
 
 function getPassword(value){
-   const password = value
-   console.log(password)
+   userInfos.pass = value
+   userInput.removeEventListener('keyup', handleKeyup10)
+   sendBtn.removeEventListener('click', handleClick10)
+   requestRecovery()
+}
 
-   userInfos.pass = password
+function requestRecovery(){
+   const request = setTimeout(function(){
+      sendSistemMsg(messages.msg15)
+      userInput.setAttribute('placeholder', 'Digite uma frase para recuperação')
+      userInput.setAttribute('type', 'text')
+      userInput.removeAttribute('maxlength')
+      userInput.addEventListener('keyup', handleKeyup11)
+      sendBtn.addEventListener('click', handleClick11)
+      clearTimeout(request)
+   }, 1000)
+}
+
+function getRecovery(value){
+   userInfos.recovery = value
+   userInput.removeEventListener('keyup', handleKeyup11)
+   sendBtn.removeEventListener('click', handleClick11)
+
    console.log(userInfos)
 
-   // complete callback
+   // ending
 }
 
 function initChat(){
